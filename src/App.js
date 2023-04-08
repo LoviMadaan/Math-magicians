e70c4b35338f2f8cb67a59f7d19b5a29b56f1449
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-04-08 17:05:30 +0530

    API and navbar updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-/* eslint-disable */
-import Calculator from './components/calculator';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Quotes from './components/Quotes';
+import Calculator from './components/Calculator';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <NavBar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quotes />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Keypads.js
+++ src/Components/Keypads.js
@@ -1,105 +1,108 @@
 import PropTypes from 'prop-types';
 
-const Keypads = ({ handleOnClick, state }) => {
+const Keypads = ({ handleClick, state }) => {
   const { total, next, operation } = state;
   return (
-    <div className="keypads">
-      <p>
-        {total}
-        {' '}
-        {operation}
-        {' '}
-        {next}
-      </p>
-      <div className="keypads-btn">
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          AC
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          +/-
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          %
-        </button>
-        <button
-          onClick={(e) => handleOnClick(e)}
-          type="button"
-          className="bg-orange operation"
-        >
-          ÷
-        </button>
-        <br />
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          7
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          8
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          9
-        </button>
-        <button
-          onClick={(e) => handleOnClick(e)}
-          type="button"
-          className="bg-orange operation"
-        >
-          x
-        </button>
-        <br />
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          4
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          5
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          6
-        </button>
-        <button
-          onClick={(e) => handleOnClick(e)}
-          type="button"
-          className="bg-orange operation"
-        >
-          -
-        </button>
-        <br />
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          1
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          2
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          3
-        </button>
-        <button
-          onClick={(e) => handleOnClick(e)}
-          type="button"
-          className="bg-orange operation"
-        >
-          +
-        </button>
-        <br />
-        <button onClick={(e) => handleOnClick(e)} type="button" className="zero">
-          0
-        </button>
-        <button onClick={(e) => handleOnClick(e)} type="button">
-          .
-        </button>
-        <button
-          onClick={(e) => handleOnClick(e)}
-          type="button"
-          className="bg-orange operation"
-        >
-          =
-        </button>
+    <div className="calculators-container">
+      <h2>Let&apos;s do some Math!</h2>
+      <div className="keypads">
+        <p>
+          {total}
+          {' '}
+          {operation}
+          {' '}
+          {next}
+        </p>
+        <div className="keypad-btn">
+          <button onClick={(e) => handleClick(e)} type="button">
+            AC
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            +/-
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            %
+          </button>
+          <button
+            onClick={(e) => handleClick(e)}
+            type="button"
+            className="bg-orange operation"
+          >
+            ÷
+          </button>
+          <br />
+          <button onClick={(e) => handleClick(e)} type="button">
+            7
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            8
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            9
+          </button>
+          <button
+            onClick={(e) => handleClick(e)}
+            type="button"
+            className="bg-orange operation"
+          >
+            x
+          </button>
+          <br />
+          <button onClick={(e) => handleClick(e)} type="button">
+            4
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            5
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            6
+          </button>
+          <button
+            onClick={(e) => handleClick(e)}
+            type="button"
+            className="bg-orange operation"
+          >
+            -
+          </button>
+          <br />
+          <button onClick={(e) => handleClick(e)} type="button">
+            1
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            2
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            3
+          </button>
+          <button
+            onClick={(e) => handleClick(e)}
+            type="button"
+            className="bg-orange operation"
+          >
+            +
+          </button>
+          <br />
+          <button onClick={(e) => handleClick(e)} type="button" className="zero">
+            0
+          </button>
+          <button onClick={(e) => handleClick(e)} type="button">
+            .
+          </button>
+          <button
+            onClick={(e) => handleClick(e)}
+            type="button"
+            className="bg-orange operation"
+          >
+            =
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 Keypads.propTypes = {
-  handleOnClick: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
   state: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
